65ad99b63d0b4ca003e4c8b397a352e33e18c627
Allen <allenxavier45@gmail.com>
2021-05-31 17:30:36 +0530

    example message

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Allen Joseph',
+  subtitle: 'A casual coder who loves to experiment with new things!.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Allen Joseph',
+    photo: '/photo.png',
+    bio: 'A casual coder who loves to experiment with new things!.    ',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
